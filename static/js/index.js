8a459a2bc1d5a421b27d7bd58e377a831ab1d4a1
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2015-11-23 11:16:59 -0500

    animations and fixes

--- static/js/index.js
+++ static/js/index.js
@@ -1,32 +1,41 @@
 audioIds = [];
 boxDivs = [];
 labels = [];
+// These are the keycodes for S D F G H J K
 keys = [115,100,102,103,104,106,107]
 
 $(document).ready(function() {
-    
-  var addAudio = function(array) {
-    return function(event){
-      audioIds[array.indexOf(event.target)].currentTime = 0;
-      console.log(array.indexOf(event.target))
-      audioIds[array.indexOf(event.target)].play();
-    }
-  }
 
   for (var i=0;i<7;i++) {
     audioIds.push(document.querySelectorAll('audio')[i]);
     boxDivs.push(document.querySelectorAll('.box')[i]);
-    labels.push(document.querySelectorAll('h1')[i]);
+    labels.push(document.querySelectorAll('span')[i]);
 
-    boxDivs[i].addEventListener('mousedown',addAudio(boxDivs));
-    labels[i].addEventListener('mousedown',addAudio(labels));
-
+    // Add event listeners for all 7 spans
+    labels[i].addEventListener('mousedown',function(event) {
+      // Find the index value of the target      
+      index = labels.indexOf(event.target);
+      //Add and remove pulse to the target Div
+      boxDivs[index].classList.add('pulse');
+      window.setTimeout(function() {
+        boxDivs[index].classList.remove('pulse')
+      }, 650);
+      // Reset and play audio for the target note
+      audioIds[index].currentTime = 0;
+      audioIds[index].play();
+    });
   }
 
-  window.addEventListener('keypress',function(event){
+  // Add event listener to the window with the condition to only play the corresponding note if S D F G H J K are pressed
+  window.addEventListener('keypress', function(event) {
       if (keys.indexOf(event.keyCode)>=0) {
-        audioIds[keys.indexOf(event.keyCode)].currentTime = 0;
-        audioIds[keys.indexOf(event.keyCode)].play();
+        index = keys.indexOf(event.keyCode);
+        boxDivs[index].classList.add('pulse');
+        window.setTimeout(function() {
+          boxDivs[index].classList.remove('pulse')
+        }, 650);
+        audioIds[index].currentTime = 0;
+        audioIds[index].play();
       }
     }
   )
